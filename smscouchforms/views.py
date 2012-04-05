42c5a2316af81192b65d66408bbf09b0365b5b52
Cory Zue <czue@dimagi.com>
2012-04-05 19:21:14 -0400

    make view more trigger-aware

--- smscouchforms/views.py
+++ smscouchforms/views.py
@@ -1,10 +1,11 @@
 # Create your views here.
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
-from touchforms.formplayer.models import XForm
+from smsforms.models import DecisionTrigger
+from django.db.models.aggregates import Count
 
 def download(request):
-    forms = XForm.objects.all()
+    triggers = DecisionTrigger.objects.annotate(num_sessions=Count('xformssession'))
     return render_to_response("smscouchforms/smsform_download.html",
-                              {"forms": forms}, 
+                              {"triggers": triggers}, 
                               context_instance=RequestContext(request))
