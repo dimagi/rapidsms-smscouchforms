5359b2162143cb30c27628666f3aa6ca23e0220b
Anton de Winter <adewinter@dimagi.com>
2012-04-20 08:47:06 +0200

    Made a signal fire off another signal to allow grabbing of XFormSession (from rapidsms-forms) as well as XFormInstance (from couchforms) to be grabbed at the same time.  Hoping this is a good place for it.
    Signed-off-by: Anton de Winter <adewinter@dimagi.com>

--- smscouchforms/signals.py
+++ smscouchforms/signals.py
@@ -1,7 +1,12 @@
 from smsforms.signals import form_complete
 from couchforms.util import post_xform_to_couch
+from django.dispatch import Signal
+
+xform_saved_with_session = Signal(providing_args=["session", "xform"])
 
 def save_form_to_couch(sender, session, form, **kwargs):
-    post_xform_to_couch(form)
+    xform_instance = post_xform_to_couch(form)
+    #Send out a signal with both the XFormInstance and the Session object.
+    xform_saved_with_session.send(sender="smscouchforms", session=session, xform=xform_instance)
     
 form_complete.connect(save_form_to_couch)
