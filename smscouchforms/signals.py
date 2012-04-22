76acf94543439518e3450469d581dd3651e925b4
Anton de Winter <adewinter@dimagi.com>
2012-04-22 19:59:08 +0200

    Added a 'form-error' signal.

--- smscouchforms/signals.py
+++ smscouchforms/signals.py
@@ -3,7 +3,7 @@ from couchforms.util import post_xform_to_couch
 from django.dispatch import Signal
 
 xform_saved_with_session = Signal(providing_args=["session", "xform"])
-
+xform_saved_has_error = Signal(providing_args=["session", "xform"])
 def save_form_to_couch(sender, session, form, **kwargs):
     xform_instance = post_xform_to_couch(form)
     #Send out a signal with both the XFormInstance and the Session object.
@@ -11,8 +11,10 @@ def save_form_to_couch(sender, session, form, **kwargs):
 
 def save_form_to_couch_with_error(sender, session, form, **kwargs):
     xform_instance = post_xform_to_couch(form)
+    xform_instance.has_error = True
+    xform_instance.save()
     #Send out a signal with both the XFormInstance and the Session object.
-    xform_saved_with_session.send(sender="smscouchforms", session=session, xform=xform_instance)
+    xform_saved_has_error.send(sender="smscouchforms", session=session, xform=xform_instance)
 form_complete.connect(save_form_to_couch)
 
 form_error.connect(save_form_to_couch_with_error)
