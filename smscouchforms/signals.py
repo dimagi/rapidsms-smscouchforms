243b72ec9a38a8556f617a03bd21a98789067b62
Anton de Winter <adewinter@dimagi.com>
2012-04-26 22:05:32 +0200

    Changed signals signature to pass along the Router instance (for threadless-router support)
    Signed-off-by: Anton de Winter <adewinter@dimagi.com>

--- smscouchforms/signals.py
+++ smscouchforms/signals.py
@@ -2,19 +2,19 @@ from smsforms.signals import form_complete, form_error
 from couchforms.util import post_xform_to_couch
 from django.dispatch import Signal
 
-xform_saved_with_session = Signal(providing_args=["session", "xform"])
-xform_saved_has_error = Signal(providing_args=["session", "xform"])
-def save_form_to_couch(sender, session, form, **kwargs):
+xform_saved_with_session = Signal(providing_args=["session", "xform", "router"])
+xform_saved_has_error = Signal(providing_args=["session", "xform", "router"])
+def save_form_to_couch(sender, session, form, router, **kwargs):
     xform_instance = post_xform_to_couch(form)
     #Send out a signal with both the XFormInstance and the Session object.
-    xform_saved_with_session.send(sender="smscouchforms", session=session, xform=xform_instance)
+    xform_saved_with_session.send(sender="smscouchforms", session=session, xform=xform_instance, router=router)
 
-def save_form_to_couch_with_error(sender, session, form, **kwargs):
+def save_form_to_couch_with_error(sender, session, form, router, **kwargs):
     xform_instance = post_xform_to_couch(form)
     xform_instance.has_error = True
     xform_instance.save()
     #Send out a signal with both the XFormInstance and the Session object.
-    xform_saved_has_error.send(sender="smscouchforms", session=session, xform=xform_instance)
-form_complete.connect(save_form_to_couch)
+    xform_saved_has_error.send(sender="smscouchforms", session=session, xform=xform_instance, router=router)
 
+form_complete.connect(save_form_to_couch)
 form_error.connect(save_form_to_couch_with_error)
