2863a83df5b3e4c408c9eefc789a0294fdc714fd
Cory Zue <czue@dimagi.com>
2012-06-08 01:12:17 -0400

    add abstract class to make integration with other apps easier

--- smscouchforms/models.py
+++ smscouchforms/models.py
@@ -1,4 +1,21 @@
+from django.db import models
+from couchforms.models import XFormInstance
+from smsforms.models import XFormsSession
 
+class FormReferenceBase(models.Model):
+    """
+    A base class that points at a form session and couch form, so you can 
+    reference them easily.
+    """
+    form_id = models.CharField(max_length=100)
+    session = models.ForeignKey(XFormsSession)
+    
+    @property
+    def form(self):
+        return XFormInstance.get(self.form_id)
+    
+    class Meta:
+        abstract = True
 
 # just to make sure these get imported once
 from . import signals
